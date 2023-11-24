7c8abb0a9029d185863b621a5a3ec150745631ff
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2023-11-24 18:07:18 +0100

    scripts to process data before analysis

---
+++ manuscript/processing/setup.R
@@ -0,0 +1,17 @@
+
+# Setup script
+
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+library(extrafont)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(terra)
+library(ordbetareg)
+library(ggnewscale)
+library(tidyterra)
+library(ggrepel)
+
+source(file.path(wd, "manuscript", "processing", "load_model_performance.R"))
