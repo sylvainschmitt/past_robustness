b96b8f12e50bbc5817583dca3b9b10a4696b396e
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2024-03-21 19:10:20 +0100

    maxent files to simulation migration

---
+++ scripts/migration/simulations/abies_alba/maxent.R
@@ -0,0 +1,34 @@
+
+#------------------------#  
+# MAXENT: tree migration #
+#------------------------#
+
+## Abies alba ##
+
+# General setup
+wd <- "E:/USERS/VanderMeersch/migration/simulations"
+source(file.path(wd, "scripts", "setup.R"))
+ncores <- 2
+res = 500 # resolution of the grid, in meters
+extent <- c(-10,30,36,66)
+starting_year <- 12000
+
+# Model setup
+sim_dir <- "E:/USERS/VanderMeersch/data/simulations/csdm/maxent/paleo/025deg/abies_alba"
+output_dir <- file.path(wd, "abies_alba", "output", 
+                        paste0("expandLDD_scprb_2km20km_fullmodel_from", starting_year), "maxent")
+save_dir <- file.path("E:/USERS/VanderMeersch/data/simulations", "csdm", "maxent", "paleo/migration",
+                      paste0("abies_alba", "_expandLDD_scprb_2km20km_fullmodel_from", starting_year))
+
+# Species parameter
+best_threshold <- 0.341
+source(file.path(wd, "scripts", "abies_parameters.R"))
+gc()
+
+# Simulate migration
+source(file.path(wd, "scripts", "run_migration.R"))
+
+# Save migration outputs
+ncores <- 2
+source(file.path(wd, "scripts", "save_migration.R"))
+
---
+++ scripts/migration/simulations/quercus_deciduous/maxent.R
@@ -0,0 +1,35 @@
+
+#------------------------#  
+# MAXENT: tree migration #
+#------------------------#
+
+## Quercus (deciduous) ##
+
+# General setup
+wd <- "E:/USERS/VanderMeersch/migration/simulations"
+source(file.path(wd, "scripts", "setup.R"))
+ncores <- 10
+res = 500 # resolution of the grid, in meters
+extent <- c(-10,30,36,66)
+starting_year <- 11750
+
+# Model setup
+sim_dir_qrobur <- "E:/USERS/VanderMeersch/data/simulations/csdm/maxent/paleo/025deg/quercus_robur"
+sim_dir_qpetraea <- "E:/USERS/VanderMeersch/data/simulations/csdm/maxent/paleo/025deg/quercus_petraea"
+output_dir <- file.path(wd, "quercus_deciduous", "output", 
+                        paste0("expandLDD_scprb_2km20km_fullmodel_from", starting_year), "maxent")
+save_dir <- file.path("E:/USERS/VanderMeersch/data/simulations", "csdm", "maxent", "paleo/migration",
+                      paste0("quercus_deciduous", "_expandLDD_scprb_2km20km_fullmodel_from", starting_year))
+
+# Species parameter
+best_threshold_qrobur <- 0.511
+best_threshold_qpetraea <- 0.335
+source(file.path(wd, "scripts", "quercusdeciduous_parameters.R"))
+gc()
+
+# Simulate migration
+source(file.path(wd, "scripts", "run_migration_2species.R"))
+
+# Save migration outputs
+source(file.path(wd, "scripts", "save_migration_2species.R"))
+
