c7f4af52f134e8fc55ac56f1cc1c9e6725422d29
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2025-01-02 12:14:35 +0100

    scripts to prepare data for figures

---
+++ manuscript/fig_processing/setup.R
@@ -0,0 +1,20 @@
+
+# Setup script
+
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+library(extrafont)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(terra)
+library(ordbetareg)
+library(ggnewscale)
+library(tidyterra)
+library(ggrepel)
+library(hypervolume)
+library(ggh4x)
+
+source(file.path(wd, "manuscript", "processing", "load_model_performance.R"))
+source(file.path(wd, "manuscript", "processing", "suppmat", "load_model_performance_withrep.R"))
